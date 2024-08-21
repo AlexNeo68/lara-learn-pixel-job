852441dd5d817d0221f0747abaaa012f1350b99e
Александр Неудахин <makewww68@Air-Aleksandr.localnet>
2024-08-21 07:29:07 +0300

    job cards

---
+++ resources/views/components/job-card.blade.php
@@ -0,0 +1,17 @@
+<div class="p-4 bg-white/5 rounded-xl flex flex-col text-center">
+    <div class="self-start text-sm">Laracasts</div>
+    <div class="py-8 font-bold">
+        <h3>Video producer</h3>
+        <p>Full time - From $60,000</p>
+    </div>
+
+    <div>
+        <div class="flex justify-between items-center mt-auto mb-4">
+            <x-tag>Tag</x-tag>
+            <x-tag>Tag</x-tag>
+            <x-tag>Tag</x-tag>
+        </div>
+        <img class="rounded-xl" src="http://placehold.it/42/42" alt="">
+    </div>
+</div>
+{{ $slot }}
