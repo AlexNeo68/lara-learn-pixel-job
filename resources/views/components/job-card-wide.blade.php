f3513c7f5f1bacd436891a034ecfd7f1a4d10795
Александр Неудахин <makewww68@Air-Aleksandr.localnet>
2024-08-26 08:01:41 +0300

    jobs seeder

--- resources/views/components/job-card-wide.blade.php
+++ resources/views/components/job-card-wide.blade.php
@@ -6,13 +6,13 @@
 
 
     <div class="flex-1 flex flex-col">
-        <a href="#" class="self-start text-sm text-gray-400 transition-colors duration-300">Laracasts</a>
+        <a href="/employers/{{ $job->employer->id }}"
+            class="self-start text-sm text-gray-400 transition-colors duration-300">{{ $job->employer->name }}</a>
 
-        <h3 class="group-hover:text-blue-800 text-xl mt-3 font-bold">Video producer</h3>
+        <h3 class="group-hover:text-blue-800 text-xl mt-3 font-bold">{{ $job->title }}</h3>
 
-        <p class="text-sm mt-auto text-gray-400">Full time - From $60,000</p>
+        <p class="text-sm mt-auto text-gray-400">{{ $job->shedule }} - From {{ $job->salary }}</p>
     </div>
-
 
     <div>
         @foreach ($job->tags as $tag)
